83c9d2968b2a13771beb5392cedb4469b61c67ed
garethrogers2828 <rogers2828@icloud.com>
2025-11-23 17:57:04 +0000

    Add answer variable to test and styled in css

---
+++ script.js
@@ -0,0 +1,17 @@
+const answerBox = document.getElementById("answerContainer");
+const question = {
+    question: "Who has scored more Premier League goals?",
+    answers: ["Wayne Rooney", "Sergio Aguero", "Les Ferdinand"],
+    correct: "Wayne Rooney"
+};
+
+// Clear old answers
+answerBox.innerHTML = "";
+
+// Add new buttons
+question.answers.forEach(ans => {
+    const btn = document.createElement("button");
+    btn.textContent = ans;
+    answerBox.appendChild(btn);
+});
+
